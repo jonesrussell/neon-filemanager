6b69fc19382d7e097446707cad9103e343849fb6
Russell Jones <jonesrussell42@gmail.com>
2019-12-07 14:15:02 -0500

    Return current dir list as json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[macro_use]
+extern crate serde_json;
+
+use glob::glob;
+use std::collections::HashSet;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn inner_main(path: &str) -> Result<(), Box<dyn Error>> {
+  let mut entries: HashSet<PathBuf> = HashSet::new();
+
+  for entry in glob(path).unwrap().filter_map(Result::ok) {
+    entries.insert(entry);
+  }
+
+  let result = json!({
+    "code": 200,
+    "success": true,
+    "payload": {
+        "list": entries
+    }
+  });
+
+  println!("{}", result.to_string());
+  Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+  let p = "./*";
+  inner_main(p).expect("Unable to process");
 }
