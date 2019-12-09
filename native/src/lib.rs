db100c99cedf5ea4224bb2afa82aadf6154e8d28
Russell Jones <jonesrussell42@gmail.com>
2019-12-09 11:28:59 -0500

    WIP

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -11,30 +11,32 @@ use std::collections::HashSet;
 use std::error::Error;
 use std::path::PathBuf;
 
-fn ls(path: &str) -> Result<serde_json::Value, Box<dyn Error>> {
+fn _ls(path: &str) -> Result<serde_json::Value, Box<dyn Error>> {
     let mut entries: HashSet<PathBuf> = HashSet::new();
 
     for entry in glob(path).unwrap().filter_map(Result::ok) {
         entries.insert(entry);
     }
 
-    let result = json!({
+    Ok(success(entries))
+}
+
+fn success(entries: HashSet<PathBuf>) -> serde_json::Value {
+    return json!({
       "code": 200,
       "success": true,
       "payload": {
           "list": entries
       }
     });
-
-    // println!("{}", result.to_string());
-    Ok(result)
 }
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsValue> {
-    let p = "./*";
-    let value = ls(p).expect("Unable to process");
+fn ls(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let p = cx.argument::<JsString>(0)?.value();
+    // let p = "./*";
+    let value = _ls(&String::from(p)).expect("Unable to process");
     let js_value = neon_serde::to_value(&mut cx, &value)?;
     Ok(js_value)
 }
 
-register_module!(mut cx, { cx.export_function("hello", hello) });
+register_module!(mut cx, { cx.export_function("ls", ls) });
