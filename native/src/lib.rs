8dfd87250888e66b223231a96ee1b3c1d34dfc9c
Russell Jones <jonesrussell42@gmail.com>
2019-12-08 22:13:54 -0500

    combine old main.rs with lib.rs

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,38 @@
 #[macro_use]
 extern crate neon;
 
+#[macro_use]
+extern crate serde_json;
+
+use glob::glob;
 use neon::prelude::*;
+use std::collections::HashSet;
+use std::error::Error;
+use std::path::PathBuf;
+
+fn inner_main(path: &str) -> Result<(), Box<dyn Error>> {
+    let mut entries: HashSet<PathBuf> = HashSet::new();
+
+    for entry in glob(path).unwrap().filter_map(Result::ok) {
+        entries.insert(entry);
+    }
+
+    let result = json!({
+      "code": 200,
+      "success": true,
+      "payload": {
+          "list": entries
+      }
+    });
+
+    println!("{}", result.to_string());
+    Ok(())
+}
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
+    // let p = "./*";
+    // inner_main(p).expect("Unable to process");
     Ok(cx.string("hello node"))
 }
 
-register_module!(mut cx, {
-    cx.export_function("hello", hello)
-});
+register_module!(mut cx, { cx.export_function("hello", hello) });
